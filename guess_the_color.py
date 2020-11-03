1bed2f6a2cb79431627a0c54ff805f61ddc22e0b
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2020-11-03 21:40:15 +0530

    added new lines(game description,score card, time )

--- guess_the_color.py
+++ guess_the_color.py
@@ -2,8 +2,28 @@ from tkinter import *
 import tkinter.font as font
 
 win = Tk()
-win.geometry('500x500')
+win.geometry('600x670')
 win.title('COLOR GUESSING GAME')
-header = Label(win,text='COLOR GUESSING GAME',fg='red')
+
+colors = ['RED','BLUE','BLACK','BROWN','WHITE','YELLOW','GREEN','PINK','PURPLE','VIOLET','ORANGE']
+helv40 = font.Font(family='Helvetica',size=24,weight='bold')
+ar12 = font.Font(family='Arial',size=13,weight='bold')
+
+game_description="\nGame Description: Enter the color of the word displayed below\nNote: Don't enter the text itself"
+header = Label(win,text='\nCOLOR GUESSING GAME',fg='RED',font=helv40)
 header.pack(side=TOP)
+
+sub_header=Label(win,text='\nWelcome to the game',font=font.Font(family='Arial',size=16,weight='bold'))
+sub_header.pack()
+
+des = Label(win,text=game_description,fg='grey',font=ar12)
+des.pack()
+
+game_score = Label(win,text='\nYour Score: '+ str(),fg='green',font=font.Font(family='Helvetica',size=18,weight='bold'))
+game_score.pack()
+
+
+game_time=Label(win,text='\nTime left: ',font=font.Font(family='Helvetica',size=18,weight='bold'),fg='blue')
+game_time.pack()
+
 win.mainloop()
