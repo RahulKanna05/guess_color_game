ab4b3d246087cb5721b7d65e631e5e833dffc90f
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2020-11-03 16:17:01 +0530

    created a gui box and addeda heading to it

--- guess_the_color.py
+++ guess_the_color.py
@@ -1,2 +1,9 @@
 from tkinter import *
+import tkinter.font as font
+
 win = Tk()
+win.geometry('500x500')
+win.title('COLOR GUESSING GAME')
+header = Label(win,text='COLOR GUESSING GAME',fg='red')
+header.pack(side=TOP)
+win.mainloop()
