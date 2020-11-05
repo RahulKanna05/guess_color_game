0625b103ec206abf665446c4a56ff6879a5022cc
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2020-11-05 23:31:20 +0530

    added a entry bar and start and reset buttons

--- guess_the_color.py
+++ guess_the_color.py
@@ -2,12 +2,16 @@ from tkinter import *
 import tkinter.font as font
 
 win = Tk()
-win.geometry('600x670')
+win.geometry('600x500')
 win.title('COLOR GUESSING GAME')
 
 colors = ['RED','BLUE','BLACK','BROWN','WHITE','YELLOW','GREEN','PINK','PURPLE','VIOLET','ORANGE']
 helv40 = font.Font(family='Helvetica',size=24,weight='bold')
 ar12 = font.Font(family='Arial',size=13,weight='bold')
+bottom_frame = Frame(win)
+bottom_frame.pack(side=BOTTOM)
+bottom_frame1 = Frame(win)
+bottom_frame1.pack(side=BOTTOM)
 
 game_description="\nGame Description: Enter the color of the word displayed below\nNote: Don't enter the text itself"
 header = Label(win,text='\nCOLOR GUESSING GAME',fg='RED',font=helv40)
@@ -22,8 +26,15 @@ des.pack()
 game_score = Label(win,text='\nYour Score: '+ str(),fg='green',font=font.Font(family='Helvetica',size=18,weight='bold'))
 game_score.pack()
 
-
 game_time=Label(win,text='\nTime left: ',font=font.Font(family='Helvetica',size=18,weight='bold'),fg='blue')
 game_time.pack()
+
+entry = Entry(win,width=14,font=font.Font(family='Helvetica',size=14),justify='center')
+entry.pack(pady=15,ipady=2)
+
+start = Button(bottom_frame,text='Start',width=7,font=font.Font(family='Arial',size=16),fg='red')
+reset = Button(bottom_frame,text='Reset',width=7,font=font.Font(family='Arial',size=16),fg='green')
+start.pack(side=LEFT,pady=10)
+reset.pack(side=LEFT,pady=10)
 
 win.mainloop()
